9ec68bf3cef1eb341e7fe9670ce3777bb2f2f42c
Michal Stencel <m.stencel@students.htl-leonding.ac.at>
2021-11-17 16:57:09 +0100

    added landing page frontend

---
+++ landingpage-frontend/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Desking Landingpage",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ landingpage-frontend/src/pages/index.js
@@ -0,0 +1,25 @@
+import { Link } from "gatsby";
+import * as React from "react"
+import Logo from "../images/logo.png"
+import "../style/index.scss"
+
+const Page = () => {
+
+  const link_git_text = "➞ ..und unser GitHub Repo finden Sie hier "
+
+  return (
+    <>
+    <img src={Logo} alt="Desking Logo" />
+    <h1>Wilkommen auf der Desking Landing page</h1>
+    <p>Unsere Webpräsenz wird aktuell eingerichtet. Bitte schauen Sie später vorbei! :)</p>
+    <br/>
+    <br />
+    <Link className="button tertiary" to="https://github.com/emaemaxd/desking">..und unser GitHub Repo finden Sie hier </Link>
+    <br />
+    <br />
+    <p className="footerline"><i>Desking © HTBLA Leonding, 4BHITM</i></p>
+    </>
+  );
+}
+
+export default Page
