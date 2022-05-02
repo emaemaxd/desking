2b143b410e5da1a1520e49e3fc8c9a7de63caa1f
Michal Stencel <stencel@spectory.at>
2022-05-02 10:53:54 +0200

    added stuff

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Furz</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}> 
+
+    </SafeAreaView>
   );
 }
 
