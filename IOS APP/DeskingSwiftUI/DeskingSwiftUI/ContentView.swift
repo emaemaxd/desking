a498cdcdba6d070353799b8d07beee08698898cc
emaemaxd <baumname@hotmail.com>
2022-09-20 11:43:30 +0200

    changing schema of ios app

---
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeskingSwiftUI
+//
+//  Created by Ema xd on 19.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/DeskingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskingSwiftUIApp.swift
+//  DeskingSwiftUI
+//
+//  Created by Ema xd on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DeskingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
