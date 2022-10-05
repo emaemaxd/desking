da36d10b5a8152ce97b97625b268be125b667274
emaemaxd <baumname@hotmail.com>
2022-10-05 12:34:16 +0200

    fixed start view being record time view

--- IOS APP/DeskingSwiftUI/DeskingSwiftUI/ContentView.swift
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/ContentView.swift
@@ -9,30 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
    
+    @State var selected = 3
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selected) {
             Text("Home Screen")
                 .tabItem {
                     Image(systemName: "house")
                 }
-           
+                .tag(1)
             Text("Statistiken Screen")
                 .tabItem{
                     Image(systemName: "chart.bar")
                 }
+                .tag(2)
             TrackTimeView()
                 .tabItem {
                     Image(systemName: "play.circle")
                 }
+                .tag(3)
             Text("Urlaub buchen Screen")
                 .tabItem {
                     Image(systemName: "calendar.badge.clock")
                 
                 }
+                .tag(4)
             SettingsView()
                 .tabItem {
                     Image(systemName: "gearshape")
                 }
+                .tag(5)
         }
         
     }
