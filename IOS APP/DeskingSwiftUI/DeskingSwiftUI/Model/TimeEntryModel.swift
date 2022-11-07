bb96123a2c949355a54d0fb22bb1298823c133ff
emaemaxd <baumname@hotmail.com>
2022-11-07 13:05:23 +0100

    changes to merge

---
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/Model/TimeEntryModel.swift
@@ -0,0 +1,13 @@
+//
+//  TimeEntryModel.swift
+//  DeskingSwiftUI
+//
+//  Created by Ema xd on 07.11.22.
+//
+
+import Foundation
+
+class TimeEntryModel {
+    var long = 0.0
+    var lat = 0.0
+}
