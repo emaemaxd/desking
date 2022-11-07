f6451f08fc09b8403538bebe6d11805c88b80c93
emaemaxd <baumname@hotmail.com>
2022-11-07 21:16:36 +0100

    changed method to fetch projects

---
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/Model/Project.swift
@@ -0,0 +1,23 @@
+//
+//  Project.swift
+//  DeskingSwiftUI
+//
+//  Created by Ema xd on 07.11.22.
+//
+
+import Foundation
+
+struct Project:Codable {
+    var id: Int
+    var name: String
+    var description: String
+    var customerid: Customer
+}
+
+struct Customer: Codable{
+    var id: Int
+    var firstname: String?
+    var lastname: String?
+    var email: String
+    var tel: Int
+}
