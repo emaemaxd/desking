823eb9ac7040cb1bda16903178c97309d3bc1040
emaemaxd <baumname@hotmail.com>
2022-11-07 20:36:22 +0100

    small changes

--- IOS APP/DeskingSwiftUI/DeskingSwiftUI/Model/TimeEntryModel.swift
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/Model/TimeEntryModel.swift
@@ -9,49 +9,25 @@ import Foundation
 
 
 struct Response: Codable {
-    let results: TimeEntryModel
-    let status: String
-}
-
-struct TimeEntryModel: Codable{
+    var timeEntry: OneTimeEntry
+    var timeEntryCustomer: TimeCustomer
     var firstName: String
     var lastName: String
-    var timeEntry: OneTimeEntryModel
 }
 
-struct OneTimeEntryModel:Codable {
+struct TimeCustomer: Codable{
+    var id: Float
+    var firstName: String
+    var lastName: String
+    var email: String
+    var tel: Int
+}
+
+struct OneTimeEntry:Codable {
     var timepassed: Int
     var starttime: String
-    var long: Float
-    var lat:Float
+    var longitude: Float
+    var latitude:Float
 }
 
-
-func getData(from url: String){
-    let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
-        
-        guard let data = data, error == nil else {
-            print("could not get data")
-            return
-        }
-        
-        // get data
-        var results:Response?
-        do {
-            results = try JSONDecoder().decode(Response.self, from: data)
-        } catch{
-            print("failed to convert data \(error.localizedDescription)")
-        }
-        
-        guard let json = results else {
-            return
-        }
-        
-        print(json.status)
-        print(json.results.firstName)
-        print(json.results.lastName)
-    })
-    
-    task.resume()
-}
 
