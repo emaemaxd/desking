f9447870fbb7cb0fa4379a1a370e4524df4974ef
emaemaxd <baumname@hotmail.com>
2022-11-08 08:41:29 +0100

    added new file

---
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/Views/TimeEntriesView.swift
@@ -0,0 +1,21 @@
+//
+//  TimeEntriesView.swift
+//  DeskingSwiftUI
+//
+//  Created by Ema xd on 08.11.22.
+//
+
+import SwiftUI
+
+struct TimeEntriesView: View {
+//    let selectedProj: Project?
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct TimeEntriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeEntriesView()
+    }
+}
