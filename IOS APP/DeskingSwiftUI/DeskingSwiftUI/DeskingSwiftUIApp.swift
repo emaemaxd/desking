0bb2fbf4887851ef46571e3ef73f528132e5f7a0
emaemaxd <baumname@hotmail.com>
2022-11-08 09:46:59 +0100

    Prof. Aberger fixes für @Published projects array damit die überall sind

--- IOS APP/DeskingSwiftUI/DeskingSwiftUI/DeskingSwiftUIApp.swift
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/DeskingSwiftUIApp.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 @main
 struct DeskingSwiftUIApp: App {
+    let projectVM = ProjectViewModel()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(projectVM: projectVM)
         }
     }
     
     private func setupDependencies() {
         let apiBaseUrl: String = "http://localhost:8080/api/"
+        
+        // TODO: projectVM.getData
         
         DIContainer.shared.register(type: ApiClient.self, component: DeskingApiClient(baseUrl: apiBaseUrl))
     }
