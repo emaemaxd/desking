609bcc474ff9f03dc75d3491a6ec8f6a2d2015db
maet0 <hymer122333@gmail.com>
2022-11-08 07:58:11 +0100

    Further POST Methods for Webpage

--- quarkus-jpa-start - Kopie/src/main/java/at/htl/boundary/UserResource.java
+++ quarkus-jpa-start - Kopie/src/main/java/at/htl/boundary/UserResource.java
@@ -1,14 +1,16 @@
 package at.htl.boundary;
 
+import at.htl.Models.Customer;
 import at.htl.Models.User;
 import at.htl.repository.UserRepository;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 @Path("/api/users")
@@ -28,5 +30,14 @@ public class UserResource {
     @Path("/{id}")
     public User getUser(@PathParam("id") int id){
         return ur.getUserByID(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/create/")
+    public Response createUser(User user, @Context UriInfo uriInfo) {
+        ur.persist(user);
+        URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(user.getId())).build();
+        return Response.created(uri).build();
     }
 }
