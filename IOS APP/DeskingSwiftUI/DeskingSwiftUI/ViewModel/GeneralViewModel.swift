61935c1416e48bb32dd5a0fefc1d4e92c6e61e5c
emaemaxd <baumname@hotmail.com>
2023-01-22 18:40:58 +0100

    zwischenstand

--- IOS APP/DeskingSwiftUI/DeskingSwiftUI/ViewModel/GeneralViewModel.swift
+++ IOS APP/DeskingSwiftUI/DeskingSwiftUI/ViewModel/GeneralViewModel.swift
@@ -2,5 +2,5 @@ import Foundation
 
 class GeneralViewModel: ObservableObject{
     @Published var selectedProjectName = "Desking"
-    
+    @Published var alleZeitenUrl = "http://localhost:8000/allezeiten"
 }
